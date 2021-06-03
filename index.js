02fa07450d23c8b104b7a0fd19ec5eeeb6ac842f
Luis Martinez <luis_mmartinez@live.com>
2021-06-03 18:10:13 -0400

    Done.

--- index.js
+++ index.js
@@ -20,3 +20,65 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(array) {
+  let attributes = ["firstName", "familyName", "title", "payPerHour"];
+  let employee = {};
+  attributes.map((attribute, index) => { employee[attribute] = array[index]});
+  employee["timeInEvents"] = [];;
+  employee["timeOutEvents"] = [];
+  return employee;
+}
+
+function createEmployeeRecords(array) {
+  return array.map( e => createEmployeeRecord(e));
+}
+
+function createTimeInEvent(timeInString) {
+    let newEvent = {}
+    let general = timeInString.split(" ")
+    let date = general[0];
+    let hour = parseInt(general[1], 10);
+    newEvent["type"] = "TimeIn";
+    newEvent["date"] = date;
+    newEvent["hour"] = hour;
+    this.timeInEvents.push(newEvent);
+    return this;  
+  }
+  
+  function createTimeOutEvent(timeInString) {
+    let newEvent = {}
+    let general = timeInString.split(" ")
+    let date = general[0];
+    let hour = parseInt(general[1], 10);
+    newEvent["type"] = "TimeOut";
+    newEvent["date"] = date;
+    newEvent["hour"] = hour;
+    this.timeOutEvents.push(newEvent);
+    return this;  
+  }
+
+  function hoursWorkedOnDate(date) {
+    let timeIn = this.timeInEvents.find( e => e.date === date ).hour;
+    let timeOut = this.timeOutEvents.find( e => e.date === date ).hour;
+    let hours = timeOut - timeIn;
+    let filter = hours < 1000 ? hours.toString().slice(0,1) : hours.toString().slice(0,2);
+    return parseInt(filter);
+  }
+
+  function wagesEarnedOnDate(date) {
+    return hoursWorkedOnDate.call(this, date) * this.payPerHour;
+  }
+  
+//   function allWagesFor(cRecord) {
+//     let wagesArray = cRecord.timeInEvents.map( e => wagesEarnedOnDate(cRecord, e.date ));
+//     return wagesArray.reduce((accumulator, currentValue) => currentValue + accumulator );
+//   }
+  
+  function findEmployeeByFirstName(array, name){
+    return array.find( e => e.firstName === name);
+  }
+  
+  function calculatePayroll(employees) {
+    return employees.reduce((m, e) => m + allWagesFor.call(e), 0);
+  }
